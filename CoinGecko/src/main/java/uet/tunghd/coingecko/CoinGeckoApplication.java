c832eb92e29342cf3db69b890c1308cc0139621f
TungHD <>
2023-03-07 09:26:18 +0700

    Initilize

---
+++ CoinGecko/src/main/java/uet/tunghd/coingecko/CoinGeckoApplication.java
@@ -0,0 +1,13 @@
+package uet.tunghd.coingecko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinGeckoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinGeckoApplication.class, args);
+    }
+
+}
